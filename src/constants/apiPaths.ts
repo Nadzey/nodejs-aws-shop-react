b812fd72cc587a396bbd744399036fe854238fc7
Nadzey <nadiakoluzaeva@gmail.com>
2025-03-21 13:59:25 -0400

    Path changed

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://x6rf0rm2zi.execute-api.us-east-1.amazonaws.com/prod/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://teck4fcb32.execute-api.us-east-1.amazonaws.com/prod",
+  import: "https://6iudvjpose.execute-api.us-east-1.amazonaws.com/prod/import",
   bff: "https://x6rf0rm2zi.execute-api.us-east-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
