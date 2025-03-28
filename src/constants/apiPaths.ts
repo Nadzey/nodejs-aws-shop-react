6743b5d064d1067296892e1c158a060bfb97914e
Nadzey <nadiakoluzaeva@gmail.com>
2025-03-28 15:15:05 -0400

    Cart link added

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://x6rf0rm2zi.execute-api.us-east-1.amazonaws.com/prod/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://6iudvjpose.execute-api.us-east-1.amazonaws.com/prod/import",
+  import: "https://3ogkpxjl60.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://x6rf0rm2zi.execute-api.us-east-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://3q6cynlsui.execute-api.us-east-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
