5f38c899a5e9d06bb18f2b7b812dd2ea71527e9d
Nadzey <nadiakoluzaeva@gmail.com>
2025-04-02 13:49:00 -0400

    Path for cart and order service updated

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   product: "https://x6rf0rm2zi.execute-api.us-east-1.amazonaws.com/prod/products",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://3q6cynlsui.execute-api.us-east-1.amazonaws.com/prod",
   import: "https://3ogkpxjl60.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://x6rf0rm2zi.execute-api.us-east-1.amazonaws.com/prod",
   cart: "https://3q6cynlsui.execute-api.us-east-1.amazonaws.com/prod",
